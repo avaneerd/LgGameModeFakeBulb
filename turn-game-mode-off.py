368396dc8924840f3f29ff214f75d6f84930164f
Alexander van Eerd <a.van.eerd@gmail.com>
2017-12-30 21:32:47 +0100

    initial version

---
+++ turn-game-mode-off.py
@@ -0,0 +1,12 @@
+import os
+from lg import Remote
+
+address = Remote.find_tvs(first_only=True)
+
+remote = Remote(address)
+
+remote.set_pairing_key(os.environ['LG_PAIRING_KEY'])
+
+commands = [410, 13, 13, 20, 412]
+
+remote.send_multiple(commands, 1)
